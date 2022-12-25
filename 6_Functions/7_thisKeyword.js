c10e9ed583a0ad263b2c8cb1f60a670a2bc2c026
Adnan Khan <Adnan.Khan2@ocean.ibm.com>
2022-12-25 22:18:37 +0500

    Study the this keyword in javascript

--- 6_Functions/7_thisKeyword.js
+++ 6_Functions/7_thisKeyword.js
@@ -11,31 +11,31 @@
 // * 1 => method => object
 // * 2 => function => global in node and window in browser
 
-const video = {
-  title: "a",
-  play() {
-    console.log(this);
-  },
-};
+// const video = {
+//   title: "a",
+//   play() {
+//     console.log(this);
+//   },
+// };
 
-video.play();
+// video.play();
 
 // * So in this example, because play is a method in the video object, so "this" keyword references this(video object) object itself
 
 // Now we can add a method later in this object and we will get the same result.
-video.stop = function () {
-  console.log(this);
-};
+// video.stop = function () {
+//   console.log(this);
+// };
 
 // * Now see video object on the console
-video.stop();
+// video.stop();
 // * Again stop is a method in the video object, so "this" keyword references this video object.
 
 // ! This is the example of the first rule. 1 => method => object
 
 // ! Second Rule
 // * 2 => A regular function => global in node and window in browser
-
+// * Regular Function
 function playVideo() {
   console.log(this);
 }
@@ -70,3 +70,86 @@ const v = new Video("a");
 // ! Definition
 // * When dealing with a regular function, "this" keyword by default references the global object. BUT if you call a function using the "new" operator, which is the case for constructor functions.
 // ! "this" keyword will reference a new {} empty object
+
+// const video = {
+//   title: "a",
+//   tags: ["a", "b", "c"],
+//   showTags() {
+//     this.tags.forEach(function (tag) {
+//       console.log(this, tag);
+//     });
+//   },
+// };
+
+// In the showTags() method, use "this" keyword to get the current object
+// and then we get the tags property because this is an array.
+// We can call the forEach method and here we need to pass a callback function.
+// In each iteration this function will be called will get a tag and then we can display the tag on the console.
+
+// video.showTags();
+
+// ? But what if we want to display the title of the video next to each tag?
+// console.log(this.title, tag); with this we get the undefined with every tag.
+// console.log(this, tag); we get the window object.
+// ? It's referencing the window object but aren't we inside a video object?
+// ? Shouldn't "this" reference the video object?
+// * No, because here we are inside this callback function. This function is just a regular function. Its not a method in the video object.
+// The only method we have here is the showTags.
+
+// ! So its the global/Window object that is executing this anonymous callback function.
+// ? but How can we solve the problem and display the title of the video next to each other?
+
+// Well, we have a few different solutions for this in this particular case.
+// The forEach method has 2 parameters
+// i. The first parameter is our callback function
+// ii. The second parameter is thisArg.
+// So, we can pass an object here and "this" keyword will reference to that object.
+//  { firstName: "John" } We pass this is an second argument to the forEach method. and this will reference to this object.
+
+// const video = {
+//   title: "a",
+//   tags: ["a", "b", "c"],
+//   showTags() {
+//     this.tags.forEach(function (tag) {
+//       console.log(this, tag);
+//     },  { firstName: "John" });
+//   },
+// };
+
+// video.showTags();
+
+// ! But we want our video object. so we can pass "this" keyword here
+// We are in the showTags method so "this" keyword will reference to the current object.
+// So here we aren't inside of a callback function. We are still in the execution context of the showTags method
+
+// const video = {
+//   title: "a",
+//   tags: ["a", "b", "c"],
+//   showTags() {
+//     this.tags.forEach(function (tag) {
+//       console.log(this, tag);
+//     }, this);
+//   },
+// };
+
+// video.showTags();
+
+// ! Next to each tag we can see our video object with "this" title property.
+// So we can add the title property here and with "this"
+// We can see the title if the video object next to each tag.
+
+const video = {
+  title: "a",
+  tags: ["a", "b", "c"],
+  showTags() {
+    this.tags.forEach(function (tag) {
+      console.log(this.title, tag);
+    }, this);
+  },
+};
+
+video.showTags();
+
+// ! BUT, not all methods in JavaScript give you the ability to pass the "this" argument.
+
+// * So we have a few different solutions for that.
